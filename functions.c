f0f7489c6246bec5adde6ab9ab4d809c30efe81c
letoleto3 <yana@localhost.localdomain>
2018-06-04 23:48:20 +0300

    NEW: report

--- functions.c
+++ functions.c
@@ -5,7 +5,7 @@ void param_dif (P_gas * p_d)
   p_d->Segm_T = 1;
   p_d->Segm_X = COEF;
   p_d->Segm_Y = COEF;
-  p_d->p_ro = 10;
+  p_d->p_ro = 1;
   p_d->mu = 0.1;
 }
 
@@ -14,7 +14,7 @@ void param_she_step (P_she *p_s, P_gas *p_d, int it_t, int it_sp)
   int x, y;
   int init_x = 10;
   int init_y = 10;
-  int init_t = 11;
+  int init_t = 20;
   x = p_s->M_x = init_x * (1 << (it_sp));
   y = p_s->M_y = init_y * (1 << (it_sp));
   p_s->N   = init_t * (1 << (it_t));
